96373acc631a21242b94351b5b6d17e3e4932fbf
Võ Văn Lực <43805857+vovanluc1@users.noreply.github.com>
2024-02-12 20:30:16 +0700

    Update app.js

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 // app.use('/', indexRouter);
-app.use('/.netlify/functions/api', indexRouter);
+app.use('/.netlify/functions/app', indexRouter);
 // app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
