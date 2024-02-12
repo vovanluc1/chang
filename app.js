aa3dd91ea88d9e458135a2a7d6aeb407e00153d8
Võ Văn Lực <43805857+vovanluc1@users.noreply.github.com>
2024-02-12 17:49:46 +0700

    Update app.js

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 // app.use('/.netlify/functions/server', indexRouter);
 app.use('/users', usersRouter);
-app.use('/', (req, res) => res.sendFile(path.join(__dirname, './index.html')));
+// app.use('/', (req, res) => res.sendFile(path.join(__dirname, './index.html')));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
