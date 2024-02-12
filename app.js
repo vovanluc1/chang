ff28ae5ac4b9c12fcb74247f4a73f07354a59bc5
Võ Văn Lực <43805857+vovanluc1@users.noreply.github.com>
2024-02-12 18:00:44 +0700

    Update app.js

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const app = express();
 
 app.use(express.json());
 const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
+// const usersRouter = require('./routes/users');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -21,9 +21,9 @@ app.use(express.urlencoded({ extended: false, limit: '100mb' }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-// app.use('/.netlify/functions/server', indexRouter);
-app.use('/users', usersRouter);
+// app.use('/', indexRouter);
+app.use('/.netlify/functions/api', indexRouter);
+// app.use('/users', usersRouter);
 // app.use('/', (req, res) => res.sendFile(path.join(__dirname, './index.html')));
 
 // catch 404 and forward to error handler
